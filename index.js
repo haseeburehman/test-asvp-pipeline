1d9fb5125d9f26042f1feb8b213db21eede9ed5d
CCHaseebburrehman <work.haseeburrehman@gmail.com>
2026-04-24 17:31:00 +0500

    more files

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const userInput = req.query.input;
+const userInput = req.query.input; // UNSAFE: potential for XSS
 db.query(`SELECT * FROM users WHERE name = '${userInput}'`); // UNSAFE: potential for SQL injection
-const apiKey = "12345-abcde"; 
+const apiKey = "12345-abcde"; // HARD-CODED SECRET
