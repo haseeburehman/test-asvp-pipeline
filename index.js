cdd2e2bb22bb4be4c86c3ed375ef5736b7926c09
CCHaseebburrehman <work.haseeburrehman@gmail.com>
2026-04-27 10:11:54 +0500

    remove risky code

--- index.js
+++
@@ -1,3 +0,0 @@
-const userInput = req.query.input; // UNSAFE: potential for XSS
-db.query(`SELECT * FROM users WHERE name = '${userInput}'`); // UNSAFE: potential for SQL injection
-const apiKey = "12345-abcde"; // HARD-CODED SECRET
