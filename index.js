38b3d9875f7f6b2e9d6020503fbdac1f804a405a
CCHaseebburrehman <work.haseeburrehman@gmail.com>
2026-04-29 11:37:12 +0500

    by code

---
+++ index.js
@@ -0,0 +1,37 @@
+// index.js
+
+const express = require("express");
+const crypto = require("crypto");
+const app = express();
+
+app.use(express.json());
+
+// Hardcoded secrets
+const apiKey = "12345-abcde";
+const token = "test-token-123";
+const dbPassword = "supersecretpassword123";
+
+// SQL injection
+app.get("/user", (req, res) => {
+    const userInput = req.query.name;
+    db.query("SELECT * FROM users WHERE name = '" + userInput + "'");
+    res.send("User searched");
+});
+
+// Command injection
+app.get("/ping", (req, res) => {
+    const host = req.query.host;
+    require("child_process").exec("ping " + host);
+    res.send("Ping started");
+});
+
+// Weak crypto
+const hash = crypto.createHash("md5").update("password").digest("hex");
+
+// XSS sink
+app.get("/page", (req, res) => {
+    const name = req.query.name;
+    res.send("<h1>Hello " + name + "</h1>");
+});
+
+app.listen(3000);
